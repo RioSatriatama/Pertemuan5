179a425f822d11c0943d1be542b0fa38d096407f
RioSatriatama <82066488+RioSatriatama@users.noreply.github.com>
2021-10-23 21:55:35 +0700

    Create Samsung.java

---
+++ Samsung.java
@@ -0,0 +1,11 @@
+public class Samsung extends Handphone {
+    public String Type_series;
+    
+    public String getType_series() {
+        return Type_series;
+    }
+
+    public void setType_series(String Type_series) {
+        this.Type_series = Type_series;
+    }
+}
