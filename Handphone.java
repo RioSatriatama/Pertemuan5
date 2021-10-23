0c82493a1ccbe0e55fd4b0d81c472370a9b7ca40
RioSatriatama <82066488+RioSatriatama@users.noreply.github.com>
2021-10-23 21:53:29 +0700

    Create Handphone.java

---
+++ Handphone.java
@@ -0,0 +1,29 @@
+public class Handphone {
+    public String Kamera;
+    public double Layar;
+    public String Ram;
+    
+    public String getKamera() {
+        return Kamera;
+    }
+
+    public void setKamera(String Kamera) {
+        this.Kamera = Kamera;
+    }
+
+    public double getLayar() {
+        return Layar;
+    }
+
+    public void setLayar(double Layar) {
+        this.Layar = Layar;
+    }   
+
+    public void setRam(String x){
+        this.Ram = x;
+    }
+
+    public String getRam(){
+        return this.Ram;
+    }
+}
